df0d1c26cd0fd414e925849d75865117e0a10b28
Gaurav K H <gaurav.k@learner.manipal.edu>
2021-07-08 12:16:11 +0530

    Added custom code for 3 incomplete functions

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -35,28 +35,51 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		int emptyCellId = game.getEmptyCellId();
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
-		
-		//Your logic here		
+
+		// Y co ordinates i.e row numbers are multiples of 46 and X co ordinates i.e. column nos are multiples of 47
+		// So normal co ordinates are dervied and for a 4x4 matrix the clicked button location is calculated
+		int buttonClickedId = (buttonClicked.getY()/47) * 4 + buttonClicked.getX()/46;
+
+		//Checking if the clicked button is in the same row or the same column to empty cell and adjacent to the empty cell
+		if((buttons[emptyCellId].getX() == buttonClicked.getX() && (buttons[emptyCellId].getY() == buttonClicked.getY() + 47 || buttons[emptyCellId].getY() == buttonClicked.getY() - 47)) || (buttons[emptyCellId].getY() == buttonClicked.getY() && (buttons[emptyCellId].getX() == buttonClicked.getX() + 46 || buttons[emptyCellId].getX() == buttonClicked.getX() - 46))) {
+			swapButton(buttons[emptyCellId], buttonClicked);
+			System.out.println(buttonClickedId+"\t"+emptyCellId);
+			emptyCellId = buttonClickedId;
+		}
 		
 		return emptyCellId;
-
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
-		
-		//Your logic here
-		int a = getRandomNumber();
-		
+		int j = 0;
+		while (j < 15) {
+			int a = getRandomNumber() % 15 + 1; // Generating number between 1 and 15
+			boolean present = false;
+			for (int i = 0; i < j; i++) {
+				// Checking if the generated number is already present
+				if(arr[i] == a) {
+					present = true;
+					break;
+				}
+			}
+			if(!present) {
+				arr[j] = a;
+				j++;
+			}
+		}
 		
 		return arr;
 	}
 	public boolean checkForWinner(Button[] buttons)
 	{
 		boolean winner = true;
-		
-		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
+		int arr[] = getIntegerArrayOfButtonIds(buttons);
+		for(int i = 0; i <= 15; i++) {
+			if(arr[i] != i + 1){
+				winner = false;
+				break;
+			}
+		}
 		return winner;
 	}
 }
